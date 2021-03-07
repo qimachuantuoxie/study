f003586918d06e9713987c914e690eb7e05aaf37
qimachuantuoxie <mengjiqi230@gmail.com>
2021-03-07 18:05:07 +0800

    “第一次上传”

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <mengjinqi></mengjinqi>
+    <mengjinqi></mengjinqi> 
+    <mengjinqi></mengjinqi>
+    <mengjinqi></mengjinqi>
+  </div>
+
+</template>
+
+<script>
+//引入组件
+import mengjinqi from './components/mengjinqi.vue'
+//注册组件
+export default {
+  components:{
+    mengjinqi
+  }
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/mengjinqi.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>mengjinqilalllalalal{{a}}</h1>
+    <button @click="add">jiay1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      a:100
+    }
+  },
+  methods:{
+    add(){
+      this.a++
+    }
+  }
+
+}
+</script>
+
+
+<style>
+
+</style>
